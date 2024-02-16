0c5a12a233fe2ad18d4d622d2f2d58c8aeac82fe
vszholobov <seva151201@gmail.com>
2024-02-16 19:59:42 +0300

    Write logs to specified file closes #2

--- main.go
+++ main.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
 	"net/http"
+	"os"
 	"tetrisServer/server"
 
 	"github.com/gorilla/mux"
 )
 
-// https://github.com/gorilla/websocket/blob/main/examples/echo/server.go
 func main() {
+	f := openLogFile()
+	defer f.Close()
+	log.SetOutput(f)
 	customFormatter := new(logrus.TextFormatter)
 	customFormatter.TimestampFormat = "2006-01-02 15:04:05"
 	customFormatter.FullTimestamp = true
@@ -24,6 +28,16 @@ func main() {
 	router.HandleFunc("/session/create", server.CreateSession)
 	router.HandleFunc("/session/connect/{sessionId}", server.ConnectToSession)
 	router.Handle("/metrics", promhttp.Handler())
-	//http.HandleFunc("/", server.Home)
 	log.Fatal(http.ListenAndServe(*server.Addr, router))
+}
+
+func openLogFile() *os.File {
+	logFile := "tetris-log.txt"
+	log.SetReportCaller(true)
+	f, err := os.OpenFile(logFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		fmt.Println("Failed to create logfile" + logFile)
+		panic(err)
+	}
+	return f
 }
